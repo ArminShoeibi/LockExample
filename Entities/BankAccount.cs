b5d1a07bade36f8948e1cdf80dfed6601fa77151
a.shoeibi <a.shoeibi@alibaba.ir>
2021-04-02 16:13:12 +0430

    Add project files.

---
+++ Entities/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿namespace LockExample.Entities
+{
+    public class BankAccount
+    {
+        private object padLock = new();
+        public decimal Balance { get; private set; }
+        public void Deposit(decimal amount)
+        {
+            lock (padLock)
+            {
+                Balance += amount;
+            }
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            lock (padLock)
+            {
+                Balance -= amount;
+            }
+
+        }
+    }
+}
